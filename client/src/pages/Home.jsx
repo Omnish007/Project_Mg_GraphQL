f7f77e6eed77ea78804ca74b1577f15286735e61
Omnish007 <omnish007@gmail.com>
2022-11-14 16:25:04 +0530

    FE client and project

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import AddClientModal from '../components/AddClientModal'
+import AddProjectModal from '../components/AddProjectModal'
+import Client from '../components/Clients'
+import Projects from '../components/Projects'
+
+const Home = () => {
+    return (
+        <>
+            <div className="d-flex gap-3 mb-4">
+                <AddClientModal />
+                <AddProjectModal />
+            </div>
+            <Projects />
+            <hr />
+            <Client />
+        </>
+    )
+}
+
+export default Home
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors")
 const app = express();
 const connectDb = require("./config/db");
 const { graphqlHTTP } = require("express-graphql");
@@ -7,6 +8,8 @@ const schema = require("./schema/schema");
 
 //connect to db
 connectDb();
+
+app.use(cors())
 
 app.use(
     "/graphql",
